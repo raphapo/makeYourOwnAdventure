3e2ba43529c059db246f3354f3f7b70fd296f656
Raphael Ponchon <raphael.ponchon27@student.tamdistrict.org>
2024-10-17 10:00:23 -0700

    day one

---
+++ Adventure.java
@@ -0,0 +1,13 @@
+public class Adventure{
+    public static void main(String[] args) {
+        System.out.println("You are an adventurous pirate captain and the latest lord of the sea has just died,"); //intro
+        System.out.println("it is now your life mission to find the treasure and bring your fammily glory");
+        
+        System.out.println("now where do you want to go, The bar island or skull island?");//first choice
+
+
+
+
+
+    }
+}
