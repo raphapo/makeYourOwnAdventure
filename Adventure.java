1ce59dd94c7b03cfddb43f604d9c05dcbd823bda
raphapo <144717729+raphapo@users.noreply.github.com>
2024-10-20 18:58:11 +0000

    day four prgress

--- Adventure.java
+++ Adventure.java
@@ -17,7 +17,7 @@ public class Adventure{
                 
                 System.out.println("You head to the bar island." +
                 "\nAn old man tells you the tale of his grandfather, he heard the treasure was located near the typhoon."+
-                "\nwhat do you think, should you exlore the typhoon (1), go to skull island to find pirates (2) or go to the lybrary(3)");
+                "\nwhat do you think, should you exlore the typhoon (1), go to skull island to find pirates (2) or go to the  Jade lybrary(3)");//jade library = 1
                 response = sc.nextLine();
                 
                 if(response.equals("1")){
@@ -35,22 +35,44 @@ public class Adventure{
                     if(response.equals("1")){
                         System.out.println("You give the pirates a toll and they laugh, they say 'Your the first one to give us money, the rest died'"+
                         "\nYou ask kindly if they have info about the secret treasure and they say that you should look at the lybrary."+
-                        "\nYou go back to your ship with information and must decide where you go next, do you go to the Lybrary (1) or try your inside the typhoon, a ship graveyard with treasure(2)?");
+                        "\nYou go back to your ship with information and must decide where you go next, do you go to the Emerald Lybrary (1) or try your inside the typhoon, a ship graveyard with treasure(2)?");//emerald lybray is 2
                         response = sc.nextLine();
 
                         if(response.equals("1")){
-                            System.out.println("Your ship enters the golden harbor of the Great Lybrary, you are amazed that such a center of knoledge exist"+//lybrary 2
+                            System.out.println("Your ship enters the golden harbor of the Great Emerald Lybrary, you are amazed that such a center of knoledge exist"+//lybrary 2
                             "\nYou enter the Lybrary and ask the Librarian if they know anything about the ultimate treasure, They tell you they have three scrols about it, but you can only read one."+
                             "\nThe lybrarian presents you with The scroll of maps (1), the scroll of knoledge(2) and the scroll of power.(3) wich one do you chose?");
                             response = sc.nextLine();
                             if(response.equals("1")){
-                                System.out.println("You have chosen the scroll of maps");//survive
+                                System.out.println("You have chosen the scroll of maps"+
+                                "\nThe lybrain has a creepy smile on their face as they leed you down a corridor, they annonce 'here it is' "+
+                                "\nYou pick up the scrool and feel power flowing though you viens, you exclaim 'this is it, we found it' "+
+                                "\nYou head back to your ship with the knowledge of the treasure in mind, you tell everyone to head to turtle island."+
+                                "\nYour ship arrive turle island and you order one of the sailors to dig at a specific spot, his shovel hits something hard"+
+                                "\nThis is it the treasure is in your grasp, do you share it with the crew (1) or keep it to yourself(2)");//survive
+                                response= sc.nextLine();
+    
+                                if (response.equals("1")){
+                                    System.out.println("You invite your crew over and tell them take all that you can, you deserve it"+
+                                    "\nCONGRATULATIONS, YOU WON THE GAME.");
+                                }
+                                else{
+                                    System.out.println("you feel that the crew did nothing and that you alone should get the treasure, one of the sailors finds out."+
+                                    "\nthe smell of treachery fills the air, At night the crew stabs you repeatidly, your greed led you to your downfall"+
+                                    "\nYou died a greedy pirate, good game.");
+                                }//survive
                             }
                             else if(response.equals("2")){
-                                System.out.println("You have chosen the scroll of knoledge");//death
+                                System.out.println("You have chosen the scroll of knoledge"+
+                                "\nThe lybrarian leads you down a creepy corridor and annoces, here it is."+
+                                "\n You bend to pick it up and when you open it you feel your brain heating up"+
+                                "\nyou drop to your knees and scream. You chose the wrong scrool, you died, good game");//death
                             }
                             else{
-                                System.out.println("You have chosen the scroll of power.");//death
+                                System.out.println("You have chosen the scroll of power."+
+                                "\nThe lybrarian leads you down a creepy corridor and annoces, here it is."+
+                                "\n You bend to pick it up and when you open it you feel your brain heating up"+
+                                "\nyou drop to your knees and scream. You chose the wrong scrool, you died, good game");//death
                             }
                         }
                         else{
@@ -65,18 +87,40 @@ public class Adventure{
                     }
                 }
                 else{
-                    System.out.println("Your ship enters the golden harbor of the Great Lybrary, you are amazed that such a center of knoledge exist"+//lybrary 1
+                    System.out.println("Your ship enters the golden harbor of the Great Jade Lybrary, you are amazed that such a center of knoledge exist"+//lybrary 1
                     "\nYou enter the Lybrary and ask the Librarian if they know anything about the ultimate treasure, They tell you they have three scrols about it, but you can only read one."+
                     "\nThe lybrarian presents you with The scroll of maps (1), the scroll of knoledge(2) and the scroll of power.(3) wich one do you chose?");
                     response = sc.nextLine();
                     if(response.equals("1")){
-                        System.out.println("You have chosen the scroll of maps");//death
+                        System.out.println("You have chosen the scroll of maps"+
+                        "\nThe lybrarian leads you down a creepy corridor and annoces, here it is."+
+                        "\n You bend to pick it up and when you open it you feel your brain heating up"+
+                        "\nyou drop to your knees and scream. You chose the wrong scrool, you died, good game");//death
                     }
                     else if(response.equals("2")){
-                        System.out.println("You have chosen the scroll of knoledge");//survive
+                        System.out.println("You have chosen the scroll of knowledge"+
+                        "\nThe lybrain has a creepy smile on their face as they leed you down a corridor, they annonce 'here it is'"+
+                        "\nYou pick up the scrool and feel power flowing though you viens, you exclaim 'this is it, we found it' "+
+                        "\nYou head back to your ship with the knowledge of the treasure in mind, you tell everyone to head to turtle island."+
+                        "\nYour ship arrive turle island and you order one of the sailors to dig at a specific spot, his shovel hits something hard"+
+                        "\nThis is it the treasure is in your grasp, do you share it with the crew (1) or keep it to yourself(2)");//survive
+                        response= sc.nextLine();
+
+                        if (response.equals("1")){
+                            System.out.println("You invite your crew over and tell them take all that you can, you deserve it"+
+                            "\nCONGRATULATIONS, YOU WON THE GAME.");
+                        }
+                        else{
+                            System.out.println("you feel that the crew did nothing and that you alone should get the treasure, one of the sailors finds out."+
+                            "\nthe smell of treachery fills the air, At night the crew stabs you repeatidly, your greed led you to your downfall"+
+                            "\nYou died a greedy pirate, good game.");
+                        }
                     }
                     else{
-                        System.out.println("You have chosen the scroll of power.");//death
+                        System.out.println("You have chosen the scroll of power."+
+                        "\nThe lybrarian leads you down a creepy corridor and annoces, here it is."+
+                        "\n You bend to pick it up and when you open it you feel your brain heating up"+
+                        "\nyou drop to your knees and scream. You chose the wrong scrool, you died, good game");//death
                     }
                 }
             }
@@ -89,21 +133,44 @@ public class Adventure{
                 if(response.equals("1")){
                     System.out.println("You give the pirates a toll and they laugh, they say 'Your the first one to give us money, the rest died'"+
                     "\nYou ask kindly if they have info about the secret treasure and they say that you should look at the lybrary."+
-                    "\nYou go back to your ship with information and must decide where you go next, do you go to the Lybrary (1) or try your chance at a random island? (2)");
+                    "\nYou go back to your ship with information and must decide where you go next, do you go to the Ruby Lybrary (1) or try your chance at a random island? (2)");//ruby library is num 3
                     response = sc.nextLine();
                     if (response.equals("1")){
-                        System.out.println("Your ship enters the golden harbor of the Great Lybrary, you are amazed that such a center of knoledge exist"+//lybrary 3
+                        System.out.println("Your ship enters the golden harbor of the Great Ruby Lybrary, you are amazed that such a center of knowledge exist"+//lybrary 3
                         "\nYou enter the Lybrary and ask the Librarian if they know anything about the ultimate treasure, They tell you they have three scrols about it, but you can only read one."+
                         "\nThe lybrarian presents you with The scroll of maps (1), the scroll of knoledge(2) and the scroll of power.(3) wich one do you chose?");
                         response = sc.nextLine();
                         if(response.equals("1")){
-                            System.out.println("You have chosen the scroll of maps");//death
+                            System.out.println("You have chosen the scroll of maps"+
+                            "\nThe lybrarian leads you down a creepy corridor and annoces, here it is."+
+                            "\nYou bend to pick it up and when you open it you feel your brain heating up"+
+                            "\nyou drop to your knees and scream. You chose the wrong scrool, you died, good game");//death
+
                         }
                         else if(response.equals("2")){
-                            System.out.println("You have chosen the scroll of knoledge");//death
+                            System.out.println("You have chosen the scroll of knowledge"+
+                            "\nThe lybrarian leads you down a creepy corridor and annoces, here it is."+
+                            "\n You bend to pick it up and when you open it you feel your brain heating up"+
+                            "\nyou drop to your knees and scream. You chose the wrong scrool, you died, good game");//death
                         }
                         else{
-                            System.out.println("You have chosen the scroll of power.");//survive
+                            System.out.println("You have chosen the scroll of power."+
+                            "\nThe lybrain has a creepy smile on their face as they leed you down a corridor, they annonce 'here it is' "+
+                            "\nYou pick up the scrool and feel power flowing though you viens, you exclaim 'this is it, we found it' "+
+                            "\nYou head back to your ship with the knowledge of the treasure in mind, you tell everyone to head to turtle island."+
+                            "\nYour ship arrive turle island and you order one of the sailors to dig at a specific spot, his shovel hits something hard"+
+                            "\nThis is it the treasure is in your grasp, do you share it with the crew (1) or keep it to yourself(2)");//survive
+                            response= sc.nextLine();
+
+                            if (response.equals("1")){
+                                System.out.println("You invite your crew over and tell them take all that you can, you deserve it"+
+                                "\nCONGRATULATIONS, YOU WON THE GAME.");
+                            }
+                            else{
+                                System.out.println("you feel that the crew did nothing and that you alone should get the treasure, one of the sailors finds out."+
+                                "\nthe smell of treachery fills the air, At night the crew stabs you repeatidly, your greed led you to your downfall"+
+                                "\nYou died a greedy pirate, good game.");
+                            }
                         }
                     }
                     else{
